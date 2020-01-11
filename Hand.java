716e0857c56f1d4eb9d9045c508e5ecc4bf0b1c4
welbym <welbymoore0816@gmail.com>
2020-01-10 20:47:04 -0600

    Added Hand class

---
+++ Hand.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+
+// hand.java represents the cards that the player has in their hand.
+public class Hand {
+
+  // Variables
+
+  /**
+   * cards contains all the cards in the deck.
+   */
+  private List<Card> cards;
+}
