880d367f117cf4ce3c80e874ded52ebc89e33574
welbym <welbymoore0816@gmail.com>
2020-01-12 18:38:26 -0600

    Added blackjackTerminal class

---
+++ blackjackTerminal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// A game of Blackjack that is played in the Terminal
+public class blackjackTerminal {
+  public static void main(String [] args) {
+      Hand player = new Hand();
+      Hand dealer = new Hand();
+      Deck blackjack = new Deck();
+      Scanner scanIn = new Scanner(System.in);
+
+      
+  }
+}
